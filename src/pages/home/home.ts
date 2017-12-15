6719b1f26ecbd28f881b928df0df477f34dac301
Raymond Hendricks <rayjhendricks22@gmail.com>
2017-12-15 17:14:30 -0500

    add initial steps

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,29 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  date: any;
+  daysInThisMonth: any;
+  daysInLastMonth: any;
+  daysInNextMonth: any;
+  monthNames: string[];
+  currentMonth: any;
+  currentYear: any;
+  currentDate: any;
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  getDaysOfMonth() {
+    this.daysInThisMonth = new Array();
+    this.daysInLastMonth = new Array();
+    this.daysInNextMonth = new Array();
+    this.currentMonth = this.monthNames[this.date.getMonth()];
+    this.currentYear = this.date.getFullYear();
+    if(this.date.getMonth() === new Date().getMonth()) {
+      this.currentDate = new Date().getDate();
+    } else {
+      this.currentDate = 999;
+    }
+  }
 }
